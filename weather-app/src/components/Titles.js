8020819d23d14d0008372f8dda222cdb342dfaef
Enrique Rosales <erosales95@gmail.com>
2019-02-05 01:56:47 -0600

    cleaned up some of the code in Form.js and Titles.js

--- weather-app/src/components/Titles.js
+++ weather-app/src/components/Titles.js
@@ -1,17 +1,10 @@
 import React from "react"
 
-class Titles extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>
-                    Weather Finder
-                </h1>
+const Titles = () => (
+    <div>
+        <h1>Weather Finder</h1>
 
-                <p>Find out temperature and more...</p>
-            </div>
-        );
-    }
-}
-
+        <p>Find out temperature and more...</p>
+    </div>
+);
 export default Titles;
