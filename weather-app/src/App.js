9134df05c4580c3cbd92c9fd7857b977953e4b83
Enrique Rosales <erosales95@gmail.com>
2019-02-07 12:55:03 -0600

    fixed the bug if something is misspelled.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -21,16 +21,30 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`)
     const data = await api_call.json();
+    const spelling_error = data.message;
     if (city && country) {
-      console.log(data);
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-      })
+
+      if (spelling_error === "city not found") {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "Location not found. Please check spelling."
+        })
+      } else {
+        console.log(data);
+        this.setState({
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: ""
+        })
+      }
+
     } else {
       this.setState({
         temperature: undefined,
